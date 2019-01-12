f2ead1e8989b35ac9c98370dbfa2be4eed7603f5
Artur Borisovich <thesunwave@yandex.ru>
2019-01-13 01:56:09 +0300

    Add caching

--- apps/api/controllers/films/show.rb
+++ apps/api/controllers/films/show.rb
@@ -9,7 +9,9 @@ module Api
         expose :list
 
         def call(params)
-          @list = Api::Presenters::Main.new(VideoMelan::Services::Fetcher.new(kind: :get_film, movie_id: params[:id]).call)
+          @list = Cachy.cache(params[:id], expires_in: 86400) do
+            Api::Presenters::Main.new(VideoMelan::Services::Fetcher.new(kind: :get_film, movie_id: params[:id]).call)
+          end
         end
       end
     end
--- apps/api/controllers/main/show.rb
+++ apps/api/controllers/main/show.rb
@@ -9,7 +9,9 @@ module Api
         expose :list
 
         def call(params)
-          @list = Api::Presenters::Main.new(channels: VideoMelan::Services::Fetcher.new(kind: :main).call)
+          @list = Cachy.cache(:main, expires_in: 3600) do
+            Api::Presenters::Main.new(channels: VideoMelan::Services::Fetcher.new(kind: :main).call)
+          end
         end
       end
     end
